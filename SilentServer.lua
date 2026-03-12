48532c285272597197c00d035005ccb3d22bf396
MarcelineVQ <marcythevq@gmail.com>
2026-03-12 14:11:36 -0700

    [ remove ] warlock hellfire yell filter

--- SilentServer.lua
+++ SilentServer.lua
@@ -39,35 +39,10 @@ SilentServer:SetScript("OnEvent", function ()
   SilentServer[event](this,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10)
 end)
 SilentServer:RegisterEvent("ADDON_LOADED")
-SilentServer:RegisterEvent("RAID_ROSTER_UPDATE")
-
-local raid_roster = {}
-local raid_roster_count = 0
-
--- update raid members, but only if there's an actual change
-function SilentServer:RosterUpdate()
-  local rnum = GetNumRaidMembers()
-  if rnum == raid_roster_count then return end
-  raid_roster = {}
-  raid_roster_count = rnum
-  for i=1,raid_roster_count do
-    local name, _, _, _, class = GetRaidRosterInfo(i)
-    raid_roster[name] = { name = name, class = class }
-  end
-end
 
 function SilentServer:ADDON_LOADED(addon)
   if addon ~= "SilentServer" then return end
   SilentServerDB = SilentServerDB or {}
-  if SilentServerDB.hellfire == nil then SilentServerDB.hellfire = true end
-end
-
-function SilentServer:RAID_ROSTER_UPDATE()
-  self:RosterUpdate()
-end
-
-function SilentServer:PLAYER_ENTERING_WORLD()
-  self:RosterUpdate()
 end
 
 local orig_ChatFrame_OnEvent = ChatFrame_OnEvent
@@ -80,12 +55,6 @@ ChatFrame_OnEvent = function (event,a2,a3,a4,a5,a6,a7,a8,a9,a10)
       if string.find(msg,string.lower(pattern)) then
         return false
       end
-    end
-    orig_ChatFrame_OnEvent(event,a2,a3,a4,a5,a6,a7,a8,a9,a10)
-  elseif event == "CHAT_MSG_YELL" and SilentServerDB.hellfire then
-    if from and UnitName("player") ~= from and
-        raid_roster[from] and raid_roster[from].class == "Warlock" then
-      return false
     end
     orig_ChatFrame_OnEvent(event,a2,a3,a4,a5,a6,a7,a8,a9,a10)
   elseif event == "CHAT_MSG_MONSTER_YELL" then
--- readme.md
+++ readme.md
@@ -8,8 +8,5 @@ Removes:
 * Spam-like Server messages
 * Certain npc shouts/announces, in particular in cities
 
-Current version also removes Warlock /yell during raid combat, mainly due to the popularization of hellfire macros.  
-To disable this go to `WTF/Account/<accountname>/SavedVariables/SilentServer.lua` and change `["hellfire"] = true` to `["hellfire"] = false`, I'm not going to make a slash command for one variable.  
-
 ___
 * This addon is made by and for `Weird Vibes` of Turtle WoW.  
